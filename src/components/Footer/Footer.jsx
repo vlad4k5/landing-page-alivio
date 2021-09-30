da7f52e4e917f9a6e596d5e39ae6aade82936dbb
vlad4k5 <armia1823@gmail.com>
2021-10-01 02:43:35 +0300

    add some adaptive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import s from "./Footer.module.scss"
 
 const Footer = () => {
   return <div className={s.footer}>
-    <h2>Alivio</h2>
     <nav>
+    <h2>Alivio</h2>
       <ul>
         <li><button>Why Alivio</button></li>
         <li><button>Solutions</button></li>
